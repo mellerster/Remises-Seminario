b7b82ac97830546ff7b9531aec668fad010ba5b0
hugo-chavar <hechavar@gmail.com>
2014-05-16 15:42:39 -0300

    validaciones sobre reserva

--- remisesOnline/grails-app/domain/remisesonline/Reserva.groovy
+++ remisesOnline/grails-app/domain/remisesonline/Reserva.groovy
@@ -1,12 +1,16 @@
 package remisesonline
 
+import java.util.Date
+
 class Reserva {
-	Lugar destino
-	Remise remise
+	//Lugar destino
+	//Remise remise
 	Date fechaReserva
 
     static constraints = {
-		destino nullable:false
-		remise nullable:false
+//		destino nullable:false
+//		remise nullable:false
+        //fechaReserva min: new Date(), max: (new Date()) + 30
+        fechaReserva(validator: {return (it > new Date())})
     }
 }
