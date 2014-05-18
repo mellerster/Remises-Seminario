7de12cafdc122cd4b9fa8771e5ab859263b92462
Hugo Chavar <hechavar@gmail.com>
2014-05-18 13:35:45 -0300

    Mensajes de error customizados

--- remisesOnline/grails-app/domain/remisesonline/Reserva.groovy
+++ remisesOnline/grails-app/domain/remisesonline/Reserva.groovy
@@ -8,13 +8,13 @@ class Reserva {
     static constraints = {
 //		destino nullable:false
 //		remise nullable:false
-        //fechaReserva min: new Date(), max: (new Date()) + 30
-        fechaReserva(
-                validator: {
-                    Date downLimit = new Date()
-                    Date upLimit = downLimit + 30
-
-                    return (it > downLimit && it < upLimit )
-        })
+        fechaReserva min: new Date(), max: (new Date()) + 30
+//        fechaReserva(
+//                validator: {
+//                    Date downLimit = new Date()
+//                    Date upLimit = downLimit + 30
+//
+//                    return (it > downLimit && it < upLimit )
+//        })
     }
 }
