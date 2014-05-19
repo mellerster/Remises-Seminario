55ce798c08e4c5159ac5883adfb28254d19fc018
hugo-chavar <hechavar@gmail.com>
2014-05-19 14:51:20 -0300

    agrego una redireccion en el index de itinerario

--- remisesOnline/grails-app/controllers/remisesonline/ItinerarioController.groovy
+++ remisesOnline/grails-app/controllers/remisesonline/ItinerarioController.groovy
@@ -5,6 +5,10 @@ class ItinerarioController {
     def scaffold = true
 	
 	def index = {
+		redirect(action: list)
+	}
+	
+	def list = {
 		def itinerarios = Itinerario.list(sort: "descripcion", order: "asc")
 		
 		return [itinerarios: itinerarios]
