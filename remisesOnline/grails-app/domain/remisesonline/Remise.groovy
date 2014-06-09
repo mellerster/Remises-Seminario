5a9f7803a88bf878cb005d9e67b7f2392972dbec
Hugo Chavar <hechavar@gmail.com>
2014-06-08 21:35:36 -0300

    Validación al crear un remis, Chequeo que el chofer que se asigna pertenezca a la agencia. Se muestra un mensaje en caso contrario: 'El Chofer elegido no pertenece a la Agencia'

--- remisesOnline/grails-app/domain/remisesonline/Remise.groovy
+++ remisesOnline/grails-app/domain/remisesonline/Remise.groovy
@@ -10,10 +10,17 @@ class Remise {
 	
 	static constraints = {
 		patente unique: true, nullable: false, blank: false
+    agencia nullable: true
 		comodidades min: 0 
-		chofer nullable: false
+		chofer nullable: false,
+           validator: { param_chof, param_remi ->
+                Set dniChoferesPropios = []
+                dniChoferesPropios.addAll(param_remi.agencia?.choferes.collect{it.dni})
+                if (!dniChoferesPropios.contains(param_chof.dni))
+                  return ['invalid.choferInvalido']        
+           }
 		foto nullable: true, maxSize: 1024 * 1024 * 2 /* 2MB */
-		estado inList: ["Con Pasajeros", "Vacio","Reservado"]
+		estado inList: ["Con Pasajeros", "Vacio", "Reservado"]
     }
 	
 	String toString() {
