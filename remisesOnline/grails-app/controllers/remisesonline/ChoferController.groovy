b1506d33650152fe8483c750998154875d30d7e1
Hugo Chavar <hechavar@gmail.com>
2014-06-20 19:59:41 -0300

    Corrijo menu pasajero "Mis viajes" solo muestras sus viajes

--- remisesOnline/grails-app/controllers/remisesonline/ChoferController.groovy
+++ remisesOnline/grails-app/controllers/remisesonline/ChoferController.groovy
@@ -8,7 +8,7 @@ class NuevoChoferCommand {
 	String telefono
 	String direccion
 	String licencia
-  
+	
 	static constraints = {
 		dni blank: false, nullable: false, minValue: 1000000
 		nombre blank: false, nullable: false
@@ -21,102 +21,102 @@ class NuevoChoferCommand {
 @Transactional(readOnly = true)
 class ChoferController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+		static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        def agencia = Agencia.findByNombre(session.agencia.nombre)
-        def lista = agencia.choferes.toList()
-        respond lista, model: [choferInstanceCount: lista.size()]
-    }
+		def index(Integer max) {
+				params.max = Math.min(max ?: 10, 100)
+				def agencia = Agencia.findByNombre(session.agencia.nombre)
+				def lista = agencia.choferes.toList()
+				respond lista, model: [choferInstanceCount: lista.size()]
+		}
 
-    def show(Chofer choferInstance) {
-        respond choferInstance
-    }
+		def show(Chofer choferInstance) {
+				respond choferInstance
+		}
 
-    def create() {
-        respond new Chofer(params)
-    }
+		def create() {
+				respond new Chofer(params)
+		}
 
-    @Transactional
-    def save(NuevoChoferCommand choferInstanceCommand) {
-        if (choferInstanceCommand == null) {
-            notFound()
-            return
-        }
+		@Transactional
+		def save(NuevoChoferCommand choferInstanceCommand) {
+				if (choferInstanceCommand == null) {
+						notFound()
+						return
+				}
 		
-        if (choferInstanceCommand.hasErrors()) {
-            respond choferInstanceCommand.errors, view:'create'
-            return
-        }
-        Chofer chofer = new Chofer()
-        chofer.properties = choferInstanceCommand;
-        chofer.agencia =  Agencia.get(session.agencia.id)	
+				if (choferInstanceCommand.hasErrors()) {
+						respond choferInstanceCommand.errors, view:'create'
+						return
+				}
+				Chofer chofer = new Chofer()
+				chofer.properties = choferInstanceCommand;
+				chofer.agencia =	Agencia.get(session.agencia.id)	
 
-        chofer.save flush:true , failOnError:true
+				chofer.save flush:true , failOnError:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'chofer.label', default: 'Chofer'), chofer])
-                redirect chofer
-            }
-            '*' { respond chofer, [status: CREATED] }
-        }
-    }
+				request.withFormat {
+						form multipartForm {
+								flash.message = message(code: 'default.created.message', args: [message(code: 'chofer.label', default: 'Chofer'), chofer])
+								redirect chofer
+						}
+						'*' { respond chofer, [status: CREATED] }
+				}
+		}
 
-    def edit(Chofer choferInstance) {
-        respond choferInstance
-    }
+		def edit(Chofer choferInstance) {
+				respond choferInstance
+		}
 
-    @Transactional
-    def update(Chofer choferInstance) {
-        if (choferInstance == null) {
-            notFound()
-            return
-        }
+		@Transactional
+		def update(Chofer choferInstance) {
+				if (choferInstance == null) {
+						notFound()
+						return
+				}
 
-        if (choferInstance.hasErrors()) {
-            respond choferInstance.errors, view:'edit'
-            return
-        }
+				if (choferInstance.hasErrors()) {
+						respond choferInstance.errors, view:'edit'
+						return
+				}
 		
-        choferInstance.save flush:true
+				choferInstance.save flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Chofer.label', default: 'Chofer'), choferInstance])
-                redirect choferInstance
-            }
-            '*'{ respond choferInstance, [status: OK] }
-        }
-    }
+				request.withFormat {
+						form multipartForm {
+								flash.message = message(code: 'default.updated.message', args: [message(code: 'Chofer.label', default: 'Chofer'), choferInstance])
+								redirect choferInstance
+						}
+						'*'{ respond choferInstance, [status: OK] }
+				}
+		}
 
-    @Transactional
-    def delete(Chofer choferInstance) {
+		@Transactional
+		def delete(Chofer choferInstance) {
 
-        if (choferInstance == null) {
-            notFound()
-            return
-        }
+				if (choferInstance == null) {
+						notFound()
+						return
+				}
 
-        choferInstance.delete flush:true
+				choferInstance.delete flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Chofer.label', default: 'Chofer'), choferInstance])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+				request.withFormat {
+						form multipartForm {
+								flash.message = message(code: 'default.deleted.message', args: [message(code: 'Chofer.label', default: 'Chofer'), choferInstance])
+								redirect action:"index", method:"GET"
+						}
+						'*'{ render status: NO_CONTENT }
+				}
+		}
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'choferInstance.label', default: 'Chofer'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+		protected void notFound() {
+				request.withFormat {
+						form multipartForm {
+								flash.message = message(code: 'default.not.found.message', args: [message(code: 'choferInstance.label', default: 'Chofer'), params.id])
+								redirect action: "index", method: "GET"
+						}
+						'*'{ render status: NOT_FOUND }
+				}
+		}
 }
--- remisesOnline/grails-app/controllers/remisesonline/PasajeroController.groovy
+++ remisesOnline/grails-app/controllers/remisesonline/PasajeroController.groovy
@@ -8,7 +8,7 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class PasajeroController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 	
 	def logout() {
 		session.pasajero = null
@@ -29,85 +29,85 @@ class PasajeroController {
 		return [reservas: pasajeroLogueado.reservas]
 	}
 	
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Pasajero.list(params), model:[pasajeroInstanceCount: Pasajero.count()]
-    }
+	def index(Integer max) {
+			params.max = Math.min(max ?: 10, 100)
+			respond Pasajero.list(params), model:[pasajeroInstanceCount: Pasajero.count()]
+	}
 
-    def show(Pasajero pasajeroInstance) {
-        respond pasajeroInstance
-    }
+	def show(Pasajero pasajeroInstance) {
+			respond pasajeroInstance
+	}
 
 	def crear() {
 		respond new Pasajero(params)
 	}
 
-    @Transactional
-    def guardar(Pasajero pasajeroInstance) {
-        if (pasajeroInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def guardar(Pasajero pasajeroInstance) {
+			if (pasajeroInstance == null) {
+					notFound()
+					return
+			}
 
-        if (pasajeroInstance.hasErrors()) {
-            respond pasajeroInstance.errors, view:'crear'
-            return
-        }
+			if (pasajeroInstance.hasErrors()) {
+					respond pasajeroInstance.errors, view:'crear'
+					return
+			}
 
-        pasajeroInstance.save flush:true
+			pasajeroInstance.save flush:true
 
-        session.pasajero = pasajeroInstance
-		redirect action: 'listReservas', controller: 'pasajero'
-    }
+			session.pasajero = pasajeroInstance
+      redirect action: 'listReservas', controller: 'pasajero'
+	}
 
-    def edit(Pasajero pasajeroInstance) {
-        respond pasajeroInstance
-    }
+	def edit(Pasajero pasajeroInstance) {
+			respond pasajeroInstance
+	}
 
-    @Transactional
-    def update(Pasajero pasajeroInstance) {
-        if (pasajeroInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def update(Pasajero pasajeroInstance) {
+			if (pasajeroInstance == null) {
+					notFound()
+					return
+			}
 
-        if (pasajeroInstance.hasErrors()) {
-            respond pasajeroInstance.errors, view:'edit'
-            return
-        }
+			if (pasajeroInstance.hasErrors()) {
+					respond pasajeroInstance.errors, view:'edit'
+					return
+			}
 
-        pasajeroInstance.save flush:true
+			pasajeroInstance.save flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Pasajero.label', default: 'Pasajero'), pasajeroInstance.id])
-                redirect pasajeroInstance
-            }
-            '*'{ respond pasajeroInstance, [status: OK] }
-        }
-    }
+			request.withFormat {
+					form multipartForm {
+							flash.message = message(code: 'default.updated.message', args: [message(code: 'Pasajero.label', default: 'Pasajero'), pasajeroInstance.id])
+							redirect pasajeroInstance
+					}
+					'*'{ respond pasajeroInstance, [status: OK] }
+			}
+	}
 
-    @Transactional
-    def delete(Pasajero pasajeroInstance) {
+	@Transactional
+	def delete(Pasajero pasajeroInstance) {
 
-        if (pasajeroInstance == null) {
-            notFound()
-            return
-        }
+			if (pasajeroInstance == null) {
+					notFound()
+					return
+			}
 
-        pasajeroInstance.delete flush:true
-		session.pasajero = null
-        redirect action: 'login', controller: 'pasajero'
-        
-    }
+			pasajeroInstance.delete flush:true
+      session.pasajero = null
+			redirect action: 'login', controller: 'pasajero'
+				
+	}
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'pasajeroInstance.label', default: 'Pasajero'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+			request.withFormat {
+					form multipartForm {
+							flash.message = message(code: 'default.not.found.message', args: [message(code: 'pasajeroInstance.label', default: 'Pasajero'), params.id])
+							redirect action: "index", method: "GET"
+					}
+					'*'{ render status: NOT_FOUND }
+			}
+	}
 }
