e6e4b36b1a39feded80579925b36b94dcb2b56b1
mellerster <concdiego@gmail.com>
2014-06-22 16:31:40 -0300

    Seguridad para Remise, Chofer y Comodidad

--- remisesOnline/grails-app/conf/remisesonline/AgenciaFilters.groovy
+++ remisesOnline/grails-app/conf/remisesonline/AgenciaFilters.groovy
@@ -2,17 +2,36 @@ package remisesonline
 
 class AgenciaFilters {
 	def filters = {
-		agenciaOnly(controller:'(remise|chofer)',
-		action:"(index|create|edit|save|update)") {
+		agenciaOnly(controller:'(remise|chofer|comodidad)',
+		action:"(index|create|edit|save|update|delete|modificar|actualizar)") {
 			before = {
 				if(!session?.agencia){
 					flash.message = "Solo pueden ingresar agencias que han iniciado sesión"
 					redirect(controller:"agencia", action:"login")
 					return false
 				}
+				if(params.id){
+					switch (params.controller){
+						case "remise":
+						case "comodidad":
+							if(!Agencia.get(session.agencia.id).remises.id.toString().contains(params.id.toString())){
+								flash.message = "Solo puede modificar sus remises"
+								redirect(action:"index", controller:"remise")
+								return false
+							}
+							break;
+						case "chofer":
+							if(!Agencia.get(session.agencia.id).choferes.id.toString().contains(params.id.toString())){
+								flash.message = "Solo puede modificar sus choferes"
+								redirect(action:"index", controller:"chofer")
+								return false
+							}
+							break;
+					}
+				}
 			}
 		}
-		
+
 		agenciaListadoReservas(controller:"agencia", action:"(searchReservas|listReservas)"){
 			before = {
 				if(!session?.agencia){
@@ -22,7 +41,7 @@ class AgenciaFilters {
 				}
 			}
 		}
-		
+
 		agenciaAdmin(controller:'agencia',
 		action:"edit|show|delete|update") {
 			before={
