b35d8c6d55ca3781e6d8744cbabcb4a333dad5b0
mellerster <concdiego@gmail.com>
2014-06-27 18:04:02 -0300

    Quedo un println en guardarCalificacion

--- remisesOnline/grails-app/controllers/remisesonline/RemiseController.groovy
+++ remisesOnline/grails-app/controllers/remisesonline/RemiseController.groovy
@@ -150,7 +150,6 @@ class RemiseController {
 
 	@Transactional
 	def guardarCalificacion(){
-		println params
 		def remiseInstance = Remise.get(params.id)
 		if(params.puntaje){
 			Calificacion calificacion = new Calificacion(puntaje:params.puntaje)
